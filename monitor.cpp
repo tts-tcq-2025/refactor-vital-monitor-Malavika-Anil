ea3c4030c0a506ee32da43dc263a59f9d0f16a05
Malavika-Anil <malavikaanil9891@gmail.com>
2025-08-22 09:50:25 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,24 +1,41 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-void flush() {
-for (int i = 0; i < 6; i++) {
-for (int i = 0; i < 1; i++) {
-cout << "\r* "<< flush;
-sleep_for(seconds(1)); } } }
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-if (temperature > 102 || temperature < 95) {
-cout << "Temperature is critical!\n";
-flush(); }
-return 0; }
-else if (pulseRate < 60 || pulseRate > 100)
-{cout << "Pulse Rate is out of range!\n";
-flush(); }
-return 0; }
-else if (spo2 < 90)
-{cout << "Oxygen Saturation out of range!\n";
-return 0; }
-return 1; }
+using std::cout;using std::flush;using std::this_thread::sleep_for;using std::chrono::seconds;
+
+constexpr float TEMP_HIGH=102.0f;
+constexpr float TEMP_LOW=95.0f;
+constexpr int PULSE_LOW=60;
+constexpr int PULSE_HIGH=100;
+constexpr float SPO2_LOW=90.0f;
+constexpr int ALERT_BLINKS=6;
+
+void showAlertAnimation(){
+  for(int i=0;i<ALERT_BLINKS;i++){
+    cout<<"\r* "<<flush;
+    sleep_for(seconds(1));
+    cout<<"\r *"<<flush;
+    sleep_for(seconds(1));
+  }
+  cout<<"\n";
+}
+
+int vitalsOk(float temperature,float pulseRate,float spo2){
+  if(temperature>TEMP_HIGH||temperature<TEMP_LOW){
+    cout<<"Temperature is critical!\n";
+    showAlertAnimation();
+    return 0;
+  }
+  if(pulseRate<PULSE_LOW||pulseRate>PULSE_HIGH){
+    cout<<"Pulse Rate is out of range!\n";
+    showAlertAnimation();
+    return 0;
+  }
+  if(spo2<SPO2_LOW){
+    cout<<"Oxygen Saturation is out of range!\n";
+    showAlertAnimation();
+    return 0;
+  }
+  return 1;
+}
