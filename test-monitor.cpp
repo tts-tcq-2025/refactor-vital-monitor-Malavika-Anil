bb58451a801530388b19fac7d9e993205dd43239
Malavika-Anil <malavikaanil9891@gmail.com>
2025-08-22 10:02:14 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,24 +1,13 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(MonitorTest,TemperatureChecks){
-EXPECT_FALSE(vitalsOk(103.0f,75.0f,95.0f));
-EXPECT_FALSE(vitalsOk(94.0f,75.0f,95.0f));
-EXPECT_TRUE(vitalsOk(98.6f,75.0f,95.0f));
+TEST(MonitorTest,VitalsChecks){
+  EXPECT_FALSE(vitalsOk(99,102,70));   // pulse high + spo2 low
+  EXPECT_FALSE(vitalsOk(103,80,95));   // temp high
+  EXPECT_FALSE(vitalsOk(94,80,95));    // temp low
+  EXPECT_FALSE(vitalsOk(98.6,55,95));  // pulse low
+  EXPECT_FALSE(vitalsOk(98.6,75,85));  // spo2 low
+  EXPECT_TRUE(vitalsOk(98.1,70,98));   // all normal
 }
 
-TEST(MonitorTest,PulseChecks){
-EXPECT_FALSE(vitalsOk(98.6f,110.0f,95.0f));
-EXPECT_FALSE(vitalsOk(98.6f,55.0f,95.0f));
-EXPECT_TRUE(vitalsOk(98.6f,75.0f,95.0f));
-}
-
-TEST(MonitorTest,Spo2Checks){
-EXPECT_FALSE(vitalsOk(98.6f,75.0f,85.0f));
-EXPECT_TRUE(vitalsOk(98.6f,75.0f,95.0f));
-}
-
-TEST(MonitorTest,AllVitalsNormalOk){
-EXPECT_TRUE(vitalsOk(98.1f,70.0f,98.0f));
-}
 
